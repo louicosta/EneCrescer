207632f7b7646ed8a4d68ea8ee0c199a69d6403f
Louise <louicosta.silva@gmail.com>
2022-07-22 19:26:08 -0300

    Put unique on user

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
     name: { type: String, required: true, unique: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 },
 {
