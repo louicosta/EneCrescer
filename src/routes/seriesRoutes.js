23efc54a3cd5d183fed6cac8803e928d49bb75f9
Louise <louicosta.silva@gmail.com>
2022-07-24 20:37:09 -0300

    :beers: Changing routes again

--- src/routes/seriesRoutes.js
+++ src/routes/seriesRoutes.js
@@ -4,8 +4,8 @@ const controller = require('../controllers/seriesController.js');
 
 
 router.get("/series", controller.findAllSeries)
-router.get("/serie/:title", controller.findSerieByTitle)
-router.get("/serie/:ageRating", controller.findSerieByAge)
+router.get("/serie/title", controller.findSerieByTitle)
+router.get("/serie/age", controller.findSerieByAge)
 router.get("/serie/:id", controller.findSerieById)
 router.post("/series/create", controller.newSerie)
 router.patch("/series/update/:id", controller.updateSeriesById)
